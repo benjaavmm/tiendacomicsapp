7ebd2527f889c1377ef5f7da65ac7d502d3ac6f3
Benjamin <be.vallejosm@duocuc.cl>
2024-08-27 19:30:26 -0400

    principal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  comics = [
+    {
+      title: 'The Flash N°52',
+      price: '$21.990',
+      image: 'assets/img/flash.jpg',
+    },
+    {
+      title: 'Green Lantern: Tales of the Sinestro Corps',
+      price: '$19.990',
+      image: 'assets/img/linternaverde.jpg',
+    },
+    {
+      title: 'Batman',
+      price: '$23.990',
+      image: 'assets/img/batman1.jpg',
+    },
+    {
+      title: 'Dragon Ball',
+      price: '$13.990',
+      image: 'assets/img/dragonball.jpg',
+    },
+    // Agrega más cómics según sea necesario
+  ];
 
   constructor() {}
-
 }
