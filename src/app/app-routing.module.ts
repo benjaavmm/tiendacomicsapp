27728edf0aeeb71f84ec54a074f9d532d65fb28f
Benjamin <be.vallejosm@duocuc.cl>
2024-08-30 19:07:36 -0400

    no funciona menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: 'registro', loadChildren: () => import('./registro/registro.module').then(m => m.RegistroPageModule) },
   { path: 'mangas', loadChildren: () => import('./mangas/mangas.module').then( m => m.MangasPageModule)},
   { path: 'comicsmarvel', loadChildren: () => import('./comicsmarvel/comicsmarvel.module').then( m => m.ComicsmarvelPageModule)},
-  {path: 'comicsdc',loadChildren: () => import('./comicsdc/comicsdc.module').then( m => m.ComicsdcPageModule)},
+  { path: 'comicsdc',loadChildren: () => import('./comicsdc/comicsdc.module').then( m => m.ComicsdcPageModule)},
 
 
   
