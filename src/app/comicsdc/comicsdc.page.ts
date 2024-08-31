c4297de2e2eb17e35f4b341ca686c16729ba38a3
Benjamin <be.vallejosm@duocuc.cl>
2024-08-31 00:49:12 -0400

    arreglo de alerta al carrito y click en imagenes

--- src/app/comicsdc/comicsdc.page.ts
+++ src/app/comicsdc/comicsdc.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { MenuController } from '@ionic/angular';
+import { MenuController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-comicsdc',
@@ -13,54 +12,76 @@ export class ComicsdcPage {
       title: 'The Flash N°52',
       price: '$21.990',
       image: 'assets/img/flash.jpg',
-      link: '/flash1' // Ruta en lugar de enlace HTML
+      link: '/flash1',
+      quantity: 1
     },
     {
       title: 'Linterna Verde',
       price: '$19.990',
       image: 'assets/img/linternaverde.jpg',
-      link: '/linternaverde1'
+      link: '/linternaverde1',
+      quantity: 1
     },
     {
       title: 'Batman',
       price: '$23.990',
       image: 'assets/img/batman1.jpg',
-      link: '/batman1'
+      link: '/batman1',
+      quantity: 1
     },
     {
       title: 'Aquaman #14: La Marea del Terror',
       price: '$18.890',
       image: 'assets/img/aquaman1.jpg',
-      link: '/aquaman1'
+      link: '/aquaman1',
+      quantity: 1
     },
     {
       title: 'Liga de la Justicia #27: Legado',
       price: '$20.990',
       image: 'assets/img/ligadelajusticia1.jpg',
-      link: '/ligadelajusticia1'
+      link: '/ligadelajusticia1',
+      quantity: 1
     },
     {
       title: 'Supergirl #3: El Reinado de los Superhombres Cibernéticos',
       price: '$17.000',
       image: 'assets/img/Supergirl.jpg',
-      link: '/supergirl1'
+      link: '/supergirl1',
+      quantity: 1
     },
     {
       title: 'Superman #264: El Secreto del Mariscal de Campo Fantasma',
       price: '$18.800',
       image: 'assets/img/superman1.jpg',
-      link: '/superman1'
+      link: '/superman1',
+      quantity: 1
     },
     {
       title: 'Jóvenes Titanes #1: El Reinado de los Superhombres Cibernéticos',
       price: '$19.900',
       image: 'assets/img/titans.png',
-      link: '/titans1'
+      link: '/titans1',
+      quantity: 1
     }
   ];
-  constructor(private menu: MenuController) {}
+
+  constructor(private menu: MenuController, private alertCtrl: AlertController) {}
 
   openMenu() {
     this.menu.open('first');
   }
+
+  async addToCart(comic: any) {
+    const alert = await this.alertCtrl.create({
+      header: 'Añadido al Carro',
+      message: `Has añadido ${comic.quantity} de ${comic.title} al carrito.`,
+      buttons: ['OK']
+    });
+    await alert.present();
   }
+
+  navigateToComic(link: string) {
+    window.location.href = link;
+  }
+}
--- src/app/mangas/mangas.page.ts
+++ src/app/mangas/mangas.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { MenuController } from '@ionic/angular';
+import { MenuController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mangas',
@@ -13,48 +12,79 @@ export class MangasPage {
       title: 'Naruto',
       price: '$11.990',
       image: 'assets/img/naruto.jpg',
+      link: '/naruto',
+      quantity: 1
     },
     {
       title: 'Demon Slayer',
       price: '$11.990',
       image: 'assets/img/demonslayer.jpg',
+      link: '/demonslayer',
+      quantity: 1
     },
     {
       title: 'Dragon Ball',
       price: '$13.990',
       image: 'assets/img/dragonball.jpg',
+      link: '/dragonball',
+      quantity: 1
     },
     {
       title: 'Jujutsu Kaisen',
       price: '$13.990',
       image: 'assets/img/jujutsukaisen.jpg',
+      link: '/jujutsukaisen',
+      quantity: 1
     },
     {
       title: 'Tokyo Revengers',
       price: '$12.990',
       image: 'assets/img/tokyorevengers.jpg',
+      link: '/tokyorevengers',
+      quantity: 1
     },
     {
       title: 'My Hero Academia',
       price: '$11.990',
       image: 'assets/img/myheroacademia.jpg',
+      link: '/myheroacademia',
+      quantity: 1
     },
     {
       title: 'Attack On Titan',
       price: '$12.990',
       image: 'assets/img/atackontitan.jpg',
+      link: '/atackontitan',
+      quantity: 1
     },
     {
       title: 'Hunter X Hunter',
       price: '$13.990',
       image: 'assets/img/hxh.jpg',
-    },
+      link: '/hxh',
+      quantity: 1
+    }
   ];
   
-  constructor(private menu: MenuController) {}
+  constructor(private menu: MenuController, private alertCtrl: AlertController) {}
 
   openMenu() {
     this.menu.open('first');
   }
+
+  async addToCart(comic: any) {
+    const alert = await this.alertCtrl.create({
+      header: 'Añadido al Carro',
+      message: `Has añadido ${comic.quantity} de ${comic.title} al carrito.`,
+      buttons: ['OK']
+    });
+    await alert.present();
   }
-  
+
+  navigateToComic(title: string) {
+    const selectedComic = this.comics.find(comic => comic.title === title);
+    if (selectedComic) {
+      window.location.href = selectedComic.link;
+    }
+  }
+}
