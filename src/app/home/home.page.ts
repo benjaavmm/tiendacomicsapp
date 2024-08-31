8005611bfad644c2b14807d9d3e493fe72dfb63b
Benjamin <be.vallejosm@duocuc.cl>
2024-08-31 14:29:06 -0400

    barra de busqueda y info de la pag

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,58 +13,60 @@ export class HomePage {
       price: '$21.990',
       image: 'assets/img/flash.jpg',
       link: '/flash1',
-      quantity: 1,  // Añadir esta propiedad
+      quantity: 1,
     },
     {
       title: 'Green Lantern: Tales of the Sinestro Corps',
       price: '$19.990',
       image: 'assets/img/linternaverde.jpg',
       link: '/linternaverde1',
-      quantity: 1,  // Añadir esta propiedad
+      quantity: 1,
     },
     {
       title: 'Batman',
       price: '$23.990',
       image: 'assets/img/batman1.jpg',
       link: '/batman1',
-      quantity: 1,  // Añadir esta propiedad
+      quantity: 1,
     },
     {
       title: 'Dragon Ball',
       price: '$13.990',
       image: 'assets/img/dragonball.jpg',
       link: '/dragonball',
-      quantity: 1,  // Añadir esta propiedad
+      quantity: 1,
     },
     {
       title: 'Demon Slayer',
       price: '$11.990',
       image: 'assets/img/demonslayer.jpg',
       link: '/demonslayer',
-      quantity: 1,  // Añadir esta propiedad
+      quantity: 1,
     },
     {
       title: 'Batman Beyond',
       price: '$21.990',
       image: 'assets/img/batman2.jpg',
       link: '/batmanbeyond',
-      quantity: 1,  // Añadir esta propiedad
+      quantity: 1,
     },
     {
       title: 'Naruto',
       price: '$11.990',
       image: 'assets/img/naruto.jpg',
       link: '/naruto',
-      quantity: 1,  // Añadir esta propiedad
+      quantity: 1,
     },
     {
       title: 'The Amazing Spiderman',
       price: '$18.990',
       image: 'assets/img/spiderman.jpg',
       link: '/spiderman',
-      quantity: 1,  // Añadir esta propiedad
+      quantity: 1,
     },
   ];
+
+  filteredComics = [...this.comics];  // Inicializar con todos los cómics
 
   constructor(private menu: MenuController, private alertCtrl: AlertController) {}
 
@@ -83,5 +85,17 @@ export class HomePage {
 
   navigateToComic(comic: any) {
     window.location.href = comic.link;
+  }
+
+  filterComics(event: any) {
+    const searchTerm = event.target.value.toLowerCase();
+
+    if (searchTerm && searchTerm.trim() !== '') {
+      this.filteredComics = this.comics.filter((comic) => {
+        return comic.title.toLowerCase().includes(searchTerm);
+      });
+    } else {
+      this.filteredComics = [...this.comics];
+    }
   }
 }
