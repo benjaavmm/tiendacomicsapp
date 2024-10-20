54388cb22f79b80c5e9648d6f8df307214ed5f1c
Benjamin <be.vallejosm@duocuc.cl>
2024-10-19 23:10:21 -0300

    carrito en las categorias

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MenuController, AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { CartService } from '../services/cart.service'; // Asegúrate de que la ruta es correcta
 
 @Component({
   selector: 'app-home',
@@ -10,84 +12,78 @@ export class HomePage implements OnInit, OnDestroy {
   comics = [
     {
       title: 'The Flash N°52',
-      price: '$21.990',
+      price: 21990,
       image: 'assets/img/flash.jpg',
       link: '/flash1',
       quantity: 1,
     },
     {
       title: 'Green Lantern: Tales of the Sinestro Corps',
-      price: '$19.990',
+      price: 19990,
       image: 'assets/img/linternaverde.jpg',
       link: '/linternaverde1',
       quantity: 1,
     },
     {
       title: 'Batman',
-      price: '$23.990',
+      price: 23990,
       image: 'assets/img/batman1.jpg',
       link: '/batman1',
       quantity: 1,
     },
     {
       title: 'Dragon Ball',
-      price: '$13.990',
+      price: 13990,
       image: 'assets/img/dragonball.jpg',
       link: '/dragonball',
       quantity: 1,
     },
     {
       title: 'Demon Slayer',
-      price: '$11.990',
+      price: 11990,
       image: 'assets/img/demonslayer.jpg',
       link: '/demonslayer',
       quantity: 1,
     },
     {
       title: 'Batman Beyond',
-      price: '$21.990',
+      price: 21990,
       image: 'assets/img/batman2.jpg',
       link: '/batmanbeyond',
       quantity: 1,
     },
     {
       title: 'Naruto',
-      price: '$11.990',
+      price: 11990,
       image: 'assets/img/naruto.jpg',
       link: '/naruto',
       quantity: 1,
     },
     {
       title: 'The Amazing Spiderman',
-      price: '$18.990',
+      price: 18990,
       image: 'assets/img/spiderman.jpg',
       link: '/spiderman',
       quantity: 1,
     },
   ];
 
-  filteredComics = [...this.comics];  // inicializar con todos los cómics
-
+  filteredComics = [...this.comics];
   carouselImages = [
     { src: 'assets/img/marvel.png', link: '/comicsmarvel' },
     { src: 'assets/img/mangas.png', link: '/mangas' },
     { src: 'assets/img/supermancomic.jpg', link: '/comicsdc' }
   ];
   
-  navigateToPage(link: string) {
-    window.location.href = link;
-  }
-
   currentSlide = 0;
   slideInterval: any;
 
-  constructor(private menu: MenuController, private alertCtrl: AlertController) {}
+  constructor(private menu: MenuController, private alertCtrl: AlertController, private router: Router, private cartService: CartService) {}
 
   ngOnInit() {
-    // Iniciar el carrusel automático al cargar la página
     this.slideInterval = setInterval(() => {
       this.nextSlide();
-    }, 4000);  // Cambia la imagen cada 2 segundos
+    }, 4000);
   }
 
   openMenu() {
@@ -95,6 +91,7 @@ export class HomePage implements OnInit, OnDestroy {
   }
 
   async addToCart(comic: any) {
+    this.cartService.addToCart({ ...comic });
     const alert = await this.alertCtrl.create({
       header: 'Añadido al Carro',
       message: `Has añadido ${comic.quantity} de ${comic.title} al carrito.`,
@@ -102,45 +99,35 @@ export class HomePage implements OnInit, OnDestroy {
     });
     await alert.present();
   }
-  
 
   increment(comic: any) {
-    comic.quantity++;  // Aumenta la cantidad del cómic
+    comic.quantity++;
   }
 
   decrement(comic: any) {
-    if (comic.quantity > 1) { // Evita que la cantidad sea menor que 1
-      comic.quantity--;  // Disminuye la cantidad del cómic
+    if (comic.quantity > 1) {
+      comic.quantity--;
     }
   }
 
-
   navigateToComic(comic: any) {
-    window.location.href = comic.link;
+    this.router.navigate([comic.link]);
+  }
+
+  navigateToPage(link: string) {
+    this.router.navigate([link]);
   }
 
   filterComics(event: any) {
     const searchTerm = event.target.value.toLowerCase();
-
-    if (searchTerm && searchTerm.trim() !== '') {
-      this.filteredComics = this.comics.filter((comic) => {
-        return comic.title.toLowerCase().includes(searchTerm);
-      });
-    } else {
-      this.filteredComics = [...this.comics];
-    }
+    this.filteredComics = searchTerm ? this.comics.filter(comic => comic.title.toLowerCase().includes(searchTerm)) : [...this.comics];
   }
 
   nextSlide() {
     this.currentSlide = (this.currentSlide + 1) % this.carouselImages.length;
   }
-  
-  prevSlide() {
-    this.currentSlide = (this.currentSlide - 1 + this.carouselImages.length) % this.carouselImages.length;
-  }
 
   ngOnDestroy() {
-    // Detener el carrusel automático cuando se destruya el componente
     if (this.slideInterval) {
       clearInterval(this.slideInterval);
     }
